3380a385b1a632abf5dfff8275c31ff819df9868
Arif <arifmulla.in@gmail.com>
2020-11-09 17:51:19 +0530

    Package update

--- wdio.conf.js
+++ wdio.conf.js
@@ -129,7 +129,44 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone'],
+    services: [
+        'selenium-standalone', 
+        // ['crossbrowsertesting', 
+        //     {
+        //         cbtTunnel: true,
+        //         cbtTunnelOpts: {
+        //         // any additional options from cbt_tunnels
+        //         },
+        //     }
+        // ],
+        // ['image-comparison',
+        // // The options
+        // {
+        //     // Some options, see the docs for more
+        //     baselineFolder: join(process.cwd(), './tests/sauceLabsBaseline/'),
+        //     formatImageName: '{tag}-{logName}-{width}x{height}',
+        //     screenshotPath: join(process.cwd(), '.tmp/'),
+        //     savePerInstance: true,
+        //     autoSaveBaseline: true,
+        //     blockOutStatusBar: true,
+        //     blockOutToolBar: true,
+        //     // NOTE: When you are testing a hybrid app please use this setting
+        //     isHybridApp: true,
+        //     // Options for the tabbing image
+        //     tabbableOptions:{
+        //         circle:{
+        //             size: 18,
+        //             fontSize: 18,
+        //             // ...
+        //         },
+        //         line:{
+        //             color: '#ff221a', // hex-code or for example words like `red|black|green`
+        //             width: 3,
+        //         },
+        //     }
+        //     // ... more options
+        // }],
+    ],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
